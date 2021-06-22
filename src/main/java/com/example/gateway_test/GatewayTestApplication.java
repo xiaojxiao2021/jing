846ec78f746080e8fce3b221a77c9a1a7a0ebfd8
xiaojxiao2021 <xiaojxiao2021@gmail.com>
2021-06-22 16:44:04 +0800

    init

---
+++ src/main/java/com/example/gateway_test/GatewayTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayTestApplication.class, args);
+    }
+
+}
